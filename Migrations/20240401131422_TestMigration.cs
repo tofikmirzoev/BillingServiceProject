f806b0489cac707b3375921aef081d6a93f83a6c
tofikmirzoev <65915277+tofikmirzoev@users.noreply.github.com>
2024-04-01 17:41:01 +0400

    fixed docker sql connection

---
+++ Migrations/20240401131422_TestMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillingAPI.Migrations
+{
+    public partial class TestMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InitialDepositAmount",
+                table: "Deposits",
+                newName: "InitialDepositBalance");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InitialDepositBalance",
+                table: "Deposits",
+                newName: "InitialDepositAmount");
+        }
+    }
+}
